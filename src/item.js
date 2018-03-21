9e3fed9b78620f609dcc2b31f9116d23e954c23b
Guest User <Guest@Epicodus-3E.local>
2018-03-21 16:39:48 -0700

    add front end

---
+++ src/item.js
@@ -0,0 +1,71 @@
+export function item(search) {
+
+  // // //bestbuy
+  let promise = new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    let url = `https://api.bestbuy.com/v1/products(search=${search})?format=json&show=sku,upc,name,salePrice&apiKey=OoisIQjnBk1LJsHxUacZVYJt&pageSize=25`;
+    request.onload = function() {
+      if (this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(Error(request.statusText));
+      }
+    }
+    request.open("GET", url, true);
+    request.send();
+  });
+
+  promise.then(function(response) {
+    let body = JSON.parse(response);
+    console.log(body.products);
+
+    let simpleProductArray = [];
+    body.products.forEach(function(element) {
+      simpleProductArray.push(element.name + " " + element.salePrice + " " + element.upc + " " + element.sku);
+    });
+
+    $(".bestbuyoutput").append(body.products);
+
+    // simpleProductArray.forEach(function(element) {
+    //   $(".bestbuyoutput").append("<p>" + element + "</p>");
+    // });
+  }, function(error) {
+    $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+  });
+
+  // // //walmart
+  // promise = new Promise(function(resolve, reject) {
+  //   let request = new XMLHttpRequest();
+  //   let url = `http://api.walmartlabs.com/v1/search?apiKey=vwaczxtbwhzbpjk59pkrnpua&query=ipod&categoryId=3944&sort=price&order=asc&numItems=25`;
+  //   request.onload = function() {
+  //     if (this.status === 200) {
+  //       resolve(request.response);
+  //     } else {
+  //       reject(Error(request.statusText));
+  //     }
+  //   }
+  //   request.open("GET", url, true);
+  //   request.send();
+  // });
+  //
+  // promise.then(function(response) {
+  //   let body = JSON.parse(response);
+  //   console.log(body.products);
+  //
+  //   let simpleProductArray = [];
+  //   body.items.forEach(function(element) {
+  //     simpleProductArray.push(element.name + " $" + element.salePrice + " " + element.upc);
+  //   });
+  //   console.log(simpleProductArray);
+  //
+  //   simpleProductArray.forEach(function(element) {
+  //     $(".walmartoutput").append("<p>" + element + "</p>");
+  //   });
+  // }, function(error) {
+  //   $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+  // });
+
+  // });
+}
+
+// $('.showHumidity').text(`The SKU is ${body.products.sku}%`);
--- src/main.js
+++ src/main.js
@@ -1,76 +1,14 @@
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { item } from './item.js';
+
 
 $(document).ready(function() {
-  // $('#weatherLocation').click(function() {
-    // let city = $('#location').val();
-    // $('#location').val("");
-
-  // // //bestbuy
-  let promise = new Promise(function(resolve, reject) {
-    let request = new XMLHttpRequest();
-    let url = `https://api.bestbuy.com/v1/products(search=ipod)?format=json&show=sku,upc,name,salePrice&apiKey=OoisIQjnBk1LJsHxUacZVYJt&pageSize=25`;
-    request.onload = function() {
-      if (this.status === 200) {
-        resolve(request.response);
-      } else {
-        reject(Error(request.statusText));
-      }
-    }
-    request.open("GET", url, true);
-    request.send();
+  $('#submit-search').submit(function(event){
+    event.preventDefault();
+    item($('#search').val());
   });
-
-  promise.then(function(response) {
-    let body = JSON.parse(response);
-    console.log(body.products);
-
-    let simpleProductArray = [];
-    body.products.forEach(function(element) {
-      simpleProductArray.push(element.name + " " + element.salePrice + " " + element.upc + " " + element.sku);
-    });
-
-    simpleProductArray.forEach(function(element) {
-      $(".bestbuyoutput").append("<p>" + element + "</p>");
-    });
-  }, function(error) {
-    $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-  });
-
-  // // //walmart
-  promise = new Promise(function(resolve, reject) {
-    let request = new XMLHttpRequest();
-    let url = `http://api.walmartlabs.com/v1/search?apiKey=vwaczxtbwhzbpjk59pkrnpua&query=ipod&categoryId=3944&sort=price&order=asc&numItems=25`;
-    request.onload = function() {
-      if (this.status === 200) {
-        resolve(request.response);
-      } else {
-        reject(Error(request.statusText));
-      }
-    }
-    request.open("GET", url, true);
-    request.send();
-  });
-
-  promise.then(function(response) {
-    let body = JSON.parse(response);
-    console.log(body.products);
-
-    let simpleProductArray = [];
-    body.items.forEach(function(element) {
-      simpleProductArray.push(element.name + " $" + element.salePrice + " " + element.upc);
-    });
-    console.log(simpleProductArray);
-
-    simpleProductArray.forEach(function(element) {
-      $(".walmartoutput").append("<p>" + element + "</p>");
-    });
-  }, function(error) {
-    $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-  });
-
-  // });
 });
 
 // $('.showHumidity').text(`The SKU is ${body.products.sku}%`);
