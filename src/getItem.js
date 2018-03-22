174298842528e7e40eb3bc8b0047cce24104170b
Guest User <Guest@Epicodus-7D.local>
2018-03-22 14:33:08 -0700

    walmart works now wtf

--- src/getItem.js
+++ src/getItem.js
@@ -1,37 +1,32 @@
 import $ from 'jquery';
 // import fetchResults from "../src/main.js";
 
-function getItem(fetchResults, badResults, search){
+function getItem(fetchResults, badResults, search, url){
 
   let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `https://api.bestbuy.com/v1/products(search=${search})?format=json&show=sku,upc,name,salePrice&apiKey=OoisIQjnBk1LJsHxUacZVYJt&pageSize=5`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
+    let request = new XMLHttpRequest();
+
+    request.onload = function() {
+      if (this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(Error(request.statusText));
       }
-      request.open("GET", url, true);
-      request.send();
-    });
+    }
+    request.open("GET", url, true);
+    request.send();
+  });
 
-      promise.then(function(response) {
+  promise.then(function(response) {
 
-        let body = JSON.parse(response);
-        let i = 0;
-        let productArray = [];
-        body.products.forEach(function() {
-          // console.log(body.products[i]);
-          productArray.push(body.products[i]);
-          i = i +1
-        });
-        fetchResults(response);
-      }, function(error) {
-        badResults (error);
-      });
-    // });
+    let body = JSON.parse(response);
+    console.log(body);
+    let i = 0;
+    let productArray = [];
+    fetchResults(response, url);
+  }, function(error) {
+    badResults (error);
+  });
 }
 
 export { getItem };
--- src/main.js
+++ src/main.js
@@ -8,23 +8,34 @@ $(document).ready(function() {
   $('.btn-success').click(function(event){
     event.preventDefault();
     let search = $("#search").val();
-    getItem(fetchResults, badResults, search);
+    let url = `https://api.bestbuy.com/v1/products(search=${search})?format=json&show=sku,upc,name,salePrice&apiKey=OoisIQjnBk1LJsHxUacZVYJt&pageSize=5`;
+    getItem(fetchResults, badResults, search, url);
+
+    url = `http://api.walmartlabs.com/v1/search?apiKey=vwaczxtbwhzbpjk59pkrnpua&query=${search}&categoryId=3944&sort=price&order=asc&numItems=5`;
+    getItem(fetchResults, badResults, search, url);
   });
 });
 
-let fetchResults = function(response){
+let fetchResults = function(response, url){
   let items = JSON.parse(response);
   let i = 0;
-  items.products.forEach(function(item) {
-    console.log(items.products[i]);
-    $(".bestbuyoutput").append(items.products[i].name + " " + items.products[i].upc + " $" + items.products[i].salePrice + "<br>");
-    i += 1;
-  });
 
-  // $(".bestbuyoutput").append(items.product);
-  // response.forEach(function(item) {
-  //
-  // });
+  if (url.match(/walmartlabs/)) {
+    console.log(url);
+    items.items.forEach(function(item) {
+      $(".bestbuyoutput").append(items.items[i].name + " " + items.items[i].upc + " $" + items.items[i].salePrice + "<br>");
+      i += 1;
+    });
+  }
+
+  if (url.match(/bestbuy/)) {
+    items.products.forEach(function(item) {
+      console.log(items.products[i]);
+      $(".walmartoutput").append(items.products[i].name + " " + items.products[i].upc + " $" + items.products[i].salePrice + "<br>");
+      i += 1;
+    });
+  }
+
 }
 
 let badResults = function(error){
